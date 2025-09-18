84cdaf4c86246301416e91104f1ae108acb5b147
GeethaKotagiri <125591905+GeethaKotagiri@users.noreply.github.com>
2025-09-18 11:24:02 -0500

    adding the Local LLM

--- common/llm.py
+++ common/llm.py
@@ -3,11 +3,23 @@ import os
 from cnoe_agent_utils import LLMFactory
 from config.config import LLM_PROVIDER
 
+# def get_llm():
+#   """
+#     Get the LLM provider based on the configuration using cnoe-agent-utils LLMFactory.
+#     """
+#   factory = LLMFactory(
+#     provider=LLM_PROVIDER,
+#   )
+#   return factory.get_llm()
+
+from langchain_openai import ChatOpenAI
+
 def get_llm():
   """
     Get the LLM provider based on the configuration using cnoe-agent-utils LLMFactory.
     """
-  factory = LLMFactory(
-    provider=LLM_PROVIDER,
-  )
-  return factory.get_llm()
+  model = ChatOpenAI( model="meta/llama-3.1-8b-instruct", 
+           base_url="http://10.20.1.116:8000/v1", 
+           api_key="dummy-key", temperature=0.1)
+ 
+  return model
